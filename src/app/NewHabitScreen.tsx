b91cd03107b70c7226f5c500ccdcf30c639dd4bd
jrazzg <129245648+jrazzg@users.noreply.github.com>
2026-06-03 21:38:08 +0800

    Add NewHabitScreen

---
+++ src/app/NewHabitScreen.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function NewHabitScreen() {
+    return (
+        <Text>New Habit Screen</Text>
+    )
+};
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,26 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    const router = useRouter();
+
+    return (
+        <View style={styles.container}>
+            <Text>Total Habits: 3</Text>
+            <TouchableOpacity><Text>View Stats</Text></TouchableOpacity>
+
+            <Text>Habit 1</Text>
+            <Text>Habit 2</Text>
+            <Text>Habit 3</Text>
+            <TouchableOpacity onPress={() => router.navigate('/NewHabitScreen')}><Text>Add Habit</Text></TouchableOpacity>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
