0b8668d6b96fb55ad601ce259d2a327d1950e8c3
jrazzg <129245648+jrazzg@users.noreply.github.com>
2026-06-05 01:59:03 +0800

    Update NewHabitScreen.tsx

--- src/app/NewHabitScreen.tsx
+++ src/app/NewHabitScreen.tsx
@@ -1,7 +1,24 @@
-import { Text } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function NewHabitScreen() {
+    const [habitName, setHabitName] = useState<string>('');
+    const [test, setTest] = useState<string>('');
+
     return (
-        <Text>New Habit Screen</Text>
+        <View style={styles.container}>
+            <Text>Name</Text>
+            <TextInput onChangeText={val => setHabitName(val)}/>
+            <Text>Category</Text>
+            <TouchableOpacity><Text>Save</Text></TouchableOpacity>
+            <Text>Value: {test}</Text>
+        </View>
     )
 };
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: "center",
+        padding: 20,
+    }
+});
